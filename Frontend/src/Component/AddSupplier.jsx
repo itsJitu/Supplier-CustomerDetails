e36dd22398ea2c92fef4c99544b320502d688fde
Diwakar Kumar <diwakarkumar9451@gmail.com>
2025-07-24 09:56:30 +0530

    updated code

--- Frontend/src/Component/AddSupplier.jsx
+++ Frontend/src/Component/AddSupplier.jsx
@@ -63,16 +63,80 @@ function AddSupplier() {
           {/* Supplier Name & Address - Second Set */}
           <div className="name-address" style={{ marginTop: "15px" }}>
             <div>
-              <span>Supplier Name</span>
+              <span>Contact</span>
               <br />
               <input type="text"></input>
             </div>
             <div>
-              <span>Address</span>
+              <span>Current Stock</span>
               <br />
               <input type="text"></input>
             </div>
           </div>
+
+          {/* Stock in transit & Available stock */}
+          <div className="name-address" style={{ marginTop: "15px" }}>
+            <div>
+              <span>Stock in transit</span>
+              <br />
+              <input type="text" style={{backgroundColor: "#bbc4d9ff"}}></input>
+            </div>
+            <div>
+              <span>Available stock</span>
+              <br />
+              <input type="text" style={{backgroundColor: "#bbc4d9ff"}}></input>
+            </div>
+          </div>
+
+          {/* Category & unit Price */}
+           <div className="name-address" style={{ marginTop: "15px" }}>
+            <div>
+              <span>Category</span>
+              <br />
+              <input type="text"></input>
+            </div>
+            <div>
+              <span>Unit Price</span>
+              <br />
+              <input type="text"></input>
+            </div>
+          </div>
+
+          {/* Total Inventory, Lorem & Lorem */}
+           <div className="name-address" style={{ marginTop: "15px" }}>
+            <div>
+              <span>Total Inventory</span>
+              <br />
+              <input type="text" className="total-inventory-input" style={{backgroundColor: "#bbc4d9ff"}}></input>
+            </div>
+            <div>
+              <span>Lorem</span>
+              <br />
+              <input type="text" ></input>
+            </div>
+            <div>
+              <span>Lorem</span>
+              <br />
+              <input type="text"></input>
+            </div>
+          </div>
+
+          {/* Date */}
+            <div>
+              <span>Date</span>
+              <br />
+              <input type="date"></input>
+            </div>
+
+            {/* Email id */}
+
+            <div>
+              <span>
+                Email id
+              </span>
+              <br />
+              <input type="email" />
+            </div>
         </div>
       </div>
     </div> //main div
