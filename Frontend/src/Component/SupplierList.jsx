86f1c6b4585ec808b84a32dcde4217d808a0e6e5
Diwakar Kumar <diwakarkumar9451@gmail.com>
2025-07-24 12:45:35 +0530

    updated list page

--- Frontend/src/Component/SupplierList.jsx
+++ Frontend/src/Component/SupplierList.jsx
@@ -1,19 +1,30 @@
-import React from 'react'
-import './SupplierList.css'
+import React from "react";
+import "./SupplierList.css";
 import { IoManSharp } from "react-icons/io5";
+import { FiDownload } from "react-icons/fi";
 
 function SupplierList() {
   return (
-    <div className='list-page' style={{backgroundColor: "rgb(227, 233, 217)"}}>
-      <div className='container-list'>
-        <div className='list-header'>
-          <span><IoManSharp /></span>
-          <p>Suppliers list</p>
+    <div className="list-page">
+      <div className="container-list">
+        <div className="list-header">
+          <div className="left-block">
+            <span className="icon">
+              <IoManSharp />
+            </span>
+            <div className="text-block">
+              <h3>Suppliers list</h3>
+              <span>View and manage your Suppliers</span>
+            </div>
+          </div>
+
+          <div>
+            <button className="download-btn"> <FiDownload />Download csv</button>
+          </div>
         </div>
-        
       </div>
     </div>
-  )
+  );
 }
 
-export default SupplierList
+export default SupplierList;
